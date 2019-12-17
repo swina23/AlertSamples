4d70b2cdcac8d2dbb991f48b9f1551703ac7d93a
tomo.h <tomo.the.slyb@gmail.com>
2019-12-17 21:37:19 +0900

    initial commit

---
+++ AlertSamples/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AlertSamples
+//
+//  Created by tomo on 2019/12/17.
+//  Copyright © 2019 tomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button1Alert(_ sender: Any) {
+        
+    }
+    
+    @IBAction func button2Alert(_ sender: Any) {
+        
+    }
+    
+    @IBAction func button3Alert(_ sender: Any) {
+        
+    }
+}
+
