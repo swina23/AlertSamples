ac6931276d5c48ca2464b05e14e472ad3e8bf2de
tomo.h <tomo.the.slyb@gmail.com>
2019-12-17 22:33:34 +0900

    basic alert

--- AlertSamples/Alert.swift
+++ AlertSamples/Alert.swift
@@ -10,5 +10,11 @@ import Foundation
 import UIKit
 
 struct Alert {
+     private static func showBasicAlert(on vc: UIViewController, with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true)
+        
     
+    }
 }
